cbc9082716b34c4254619377ae313e248c5040c3
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-12-02 20:01:54 +0900

    condor_status 명령 기능 완성 하지만 인바운드 규칙 적용 no

--- src/awsTest.java
+++ src/awsTest.java
@@ -8,6 +8,16 @@ package aws;
  *
  */
 // Cloud_Computing_project_2016039082
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.SQLOutput;
 import java.util.Iterator;
 import java.util.List;
@@ -19,6 +29,9 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class awsTest {
 
@@ -61,7 +74,7 @@ public class awsTest {
             System.out.println("  3. start instance               4. available regions      ");
             System.out.println("  5. stop instance                6. create instance        ");
             System.out.println("  7. reboot instance              8. list images            ");
-            System.out.println("                                 99. quit                   ");
+            System.out.println("  9. condor_status               99. quit                   ");
             System.out.println("                                 10. terminate instance     ");
             System.out.println(" 11. describe instance           12. monitoring instance    ");
             System.out.println(" 13. stop monitoring             14. Find Running Instance  ");
@@ -132,7 +145,9 @@ public class awsTest {
                 case 8:
                     listImages();
                     break;
-
+                case 9:
+                    condor_status();
+                    break;
                 case 99:
                     System.out.println("bye!");
                     menu.close();
@@ -524,7 +539,45 @@ public class awsTest {
             e.getStackTrace();
         }
     }
+    public static void condor_status()
+    {
+        String host = "13.58.106.251";
+        String user = "ec2-user";
+        String privateKey = "C:\\Users\\tlsgy\\OneDrive\\바탕 화면\\학기 공부\\project\\cloud\\Cloud_2016039082.pem";
+        int port = 22; // SSH 포트 번호
 
+        try {
+            JSch jsch = new JSch();
+            jsch.addIdentity(privateKey);
 
+            Session session = jsch.getSession(user, host, port);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
 
+            // 명령어 실행
+            String command = "condor_status";
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec) channel).setCommand(command);
+
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
+
+            InputStream in = channel.getInputStream();
+            channel.connect();
+
+            // 결과 출력
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // 연결 종료
+            channel.disconnect();
+            session.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
